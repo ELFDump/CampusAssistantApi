64f1a79469103cb6849a2ea755d936d6f68a3396
UNuX <unux@git.smiesznadomena.ml>
2018-01-13 21:25:22 +0100

    Smieszna funkcja do statystyk

---
+++ application/models/counted_in_room_m.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: iks
+ * Date: 13/01/18
+ * Time: 20:02
+ */
+
+class counted_in_room_m extends MY_Model
+{
+    protected $_table_name = 'counted_in_room';
+    protected $_order_by = 'id_actual';
+
+    function __construct()
+    {
+        parent::__construct();
+    }
+}
