cdc23eef101229d84f440bff10ae0345baf28bc0
PatrykZajac <patryk.zajac0@gmail.com>
2018-01-13 21:29:11 +0100

    Add user to position

--- application/controllers/pos_log.php
+++ application/controllers/pos_log.php
@@ -32,16 +32,25 @@ class Pos_log extends User_Controller
         //var_dump($data);
         $user = $this->user_m->get_by(array("UUID" => $data->uuid), true);
         $room = $this->room_m->get_by(array("UUID" => $data->placeId), true);
+        //var_dump($room);
+        if (count($room) == 0) {
+            $this->room_m->save(array("UUID" => $data->placeId, "id_category" => 1));
+        }
         $this->pos_log_m->save(array("id_user" => $user->id, "time" => $data->time, "description" => $data->action, "room" => $room->id_room));
-        $this->actual_in_room_m->save(array("id_user" => $user->id, "room" => $room->id_room));
+        if ($data->action == "ENTER") {
+            $this->actual_in_room_m->save(array("id_user" => $user->id, "id_room" => $room->id_room));
+        } else if ($data->action == "LEAVE") {
+            $id_action = $this->actual_in_room_m->get_by(array("id_user" => $user->id), true);
+            $this->actual_in_room_m->delete($id_action->id_actual);
+        }
         //$this->data['data'] = $data;
         //$this->load->view('api', $this->data);
     }
 
     public function get()
     {
-        $this->db->join("rooms", "rooms.id_room = pos_log.room");
-        $data = $this->pos_log_m->get();
+        $this->db->join("rooms", "rooms.id_room = actual_in_room.id_room");
+        $data = $this->actual_in_room_m->get();
 
         $rooms = $this->room_m->get();
         $datas = array();
@@ -49,14 +58,11 @@ class Pos_log extends User_Controller
             $datas[$room->UUID] = 0;
         }
         foreach ($data as $r) {
-            if ($r->description == "ENTER") {
-
-                $datas[$r->UUID]++;
-            } else if ($r->description == "LEAVE") {
-                $datas[$r->UUID]--;
-            }
+            $datas[$r->UUID]++;
         }
 
         echo(json_encode($datas));
     }
+
+
 }
--- application/models/actual_in_room_m.php
+++ application/models/actual_in_room_m.php
@@ -4,7 +4,7 @@ class Actual_in_room_m extends MY_Model
 {
     protected $_table_name = 'actual_in_room';
     protected $_order_by = 'id_actual';
-
+    protected $_primary_key = 'id_actual';
     function __construct()
     {
         parent::__construct();
